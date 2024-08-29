3074762d9226deb8b13891b012815bb4321af391
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-29 11:41:33 -0700

    Titulo y Metatags - SEO Friendly

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'about-page',
@@ -7,4 +13,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './about-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AboutPageComponent {}
+export default class AboutPageComponent implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('About Page');
+
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Este es mi about page',
+    });
+
+    this.meta.updateTag({
+      name: 'og:title',
+      content: 'About Page',
+    });
+  }
+}
