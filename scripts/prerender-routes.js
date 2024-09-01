25268e34ecf9e48c3a839b1a1b370504178789ad
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-31 21:29:02 -0700

    Construir Routes basado en peticiones HTTP

--- scripts/prerender-routes.js
+++ scripts/prerender-routes.js
@@ -5,11 +5,21 @@ const TOTAL_PAGES = 5;
   const fs = require("fs");
 
   const pokemonIds = Array.from({ length: TOTAL_POKEMONS }, (_, i) => i + 1);
+
   let fileContent = pokemonIds.map((id) => `/pokemons/${id}`).join("\n");
 
   for (let i = 1; i <= TOTAL_PAGES; i++) {
     fileContent += `\n/pokemons/page/${i}`;
   }
+
+  const pokemonNameList = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${TOTAL_POKEMONS}`
+  ).then((res) => res.json());
+
+  fileContent += "\n";
+  fileContent += pokemonNameList.results
+    .map((pokemon) => `/pokemons/${pokemon.name}`)
+    .join("\n");
 
   fs.writeFileSync("routes.txt", fileContent);
 })();
