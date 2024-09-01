ee8d3b78619f681b8b24fd7a1d281ac81d31f39c
Sergio Barreras <sergiobg.isc@gmail.com>
2024-09-01 13:58:21 -0700

    Pruebas en PokemonCardComponent

--- src/app/pokemons/components/pokemon-card/pokemon-card.component.spec.ts
+++ src/app/pokemons/components/pokemon-card/pokemon-card.component.spec.ts
@@ -1,23 +1,56 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PokemonCardComponent } from './pokemon-card.component';
+import { provideRouter } from '@angular/router';
+import { Pokemon } from '../../interfaces';
+
+const mockPokemon: Pokemon = {
+  id: '25',
+  name: 'pikachu',
+};
 
 describe('PokemonCardComponent', () => {
-  let component: PokemonCardComponent;
   let fixture: ComponentFixture<PokemonCardComponent>;
+  let compiled: HTMLElement;
+  let component: PokemonCardComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PokemonCardComponent]
-    })
-    .compileComponents();
+      imports: [PokemonCardComponent],
+      providers: [provideRouter([])],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PokemonCardComponent);
+
+    fixture.componentRef.setInput('pokemon', mockPokemon);
+
+    compiled = fixture.nativeElement as HTMLElement;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the Pokemon signal inputValue', () => {
+    expect(component.pokemon()).toEqual(mockPokemon);
+  });
+
+  it('should render the pokemon name and image correctly', () => {
+    const pokemonImage = compiled.querySelector('img');
+    expect(pokemonImage).not.toBeNull();
+
+    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${mockPokemon.id}.png`;
+    expect(pokemonImage!.src).toBe(imageUrl);
+
+    expect(compiled.textContent?.trim()).toBe(mockPokemon.name);
+  });
+
+  it('should have the proper ng-reflect-router-link', () => {
+    const divElement = compiled.querySelector('div');
+
+    expect(
+      divElement?.attributes.getNamedItem('ng-reflect-router-link')?.value
+    ).toEqual(`/pokemons,${mockPokemon.name}`);
   });
 });
