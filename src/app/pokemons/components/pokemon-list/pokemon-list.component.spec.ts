f9db63e0fae372ede85d5ac9e7246c87437f9a3b
Sergio Barreras <sergiobg.isc@gmail.com>
2024-09-01 14:16:07 -0700

    Pruebas en PokemonListComponent

--- src/app/pokemons/components/pokemon-list/pokemon-list.component.spec.ts
+++ src/app/pokemons/components/pokemon-list/pokemon-list.component.spec.ts
@@ -1,23 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PokemonListComponent } from './pokemon-list.component';
+import { Pokemon } from '../../interfaces';
+import { provideRouter } from '@angular/router';
+
+const mockPokemons: Pokemon[] = [
+  { id: '25', name: 'pikachu' },
+  { id: '26', name: 'raichu' },
+];
 
 describe('PokemonListComponent', () => {
-  let component: PokemonListComponent;
   let fixture: ComponentFixture<PokemonListComponent>;
+  let compiled: HTMLElement;
+  let component: PokemonListComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PokemonListComponent]
-    })
-    .compileComponents();
+      imports: [PokemonListComponent],
+      providers: [provideRouter([])],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PokemonListComponent);
+    compiled = fixture.nativeElement as HTMLElement;
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the pokemon list with 2 pokemon-card', () => {
+    fixture.componentRef.setInput('pokemons', mockPokemons);
+    fixture.detectChanges();
+
+    expect(compiled.querySelectorAll('pokemon-card').length).toBe(
+      mockPokemons.length
+    );
+  });
+
+  it('should render "No hay pokémons"', () => {
+    fixture.componentRef.setInput('pokemons', []);
+    fixture.detectChanges();
+
+    expect(compiled.innerText).toBe('No hay pokémons');
   });
 });
